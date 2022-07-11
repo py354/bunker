e1ca541e615e8d492c6045bd534af024435b44ae
Danis Kirasirov <dbgbbu@gmail.com>
2022-07-11 17:29:54 +0300

    Добавлено сохранение настроек

--- app/src/main/java/com/example/bunker/SettingsFragment.kt
+++ app/src/main/java/com/example/bunker/SettingsFragment.kt
@@ -1,13 +1,13 @@
 package com.example.bunker
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.SeekBar
 import androidx.fragment.app.activityViewModels
-import com.example.bunker.databinding.FragmentMainMenuBinding
 import com.example.bunker.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
@@ -16,8 +16,34 @@ class SettingsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val button = view.findViewById<Button>(R.id.back)
-        button.setOnClickListener{
+
+        val sharedPref = view.context.getSharedPreferences("settings", Context.MODE_PRIVATE)
+
+        binding.musicBtn.isChecked = sharedPref.getBoolean("music", true)
+        binding.musicBtn.setOnCheckedChangeListener { compoundButton, b ->
+            with(sharedPref.edit()) {
+                putBoolean("music", b)
+                apply()
+            }
+        }
+
+        binding.seekBar.progress = sharedPref.getInt("volume", 10)
+        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
+                with(sharedPref.edit()) {
+                    putInt("volume", progress)
+                    apply()
+                }
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+        })
+
+        binding.back.setOnClickListener{
             dataModel.message.value = MainMenuFragment()
         }
     }
