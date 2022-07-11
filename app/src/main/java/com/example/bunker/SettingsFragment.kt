8efe4f4290d0568da15791fe5c06a04c84add6cd
Danis Kirasirov <dbgbbu@gmail.com>
2022-07-11 16:37:20 +0300

    Добавлены настройки (страница)

--- app/src/main/java/com/example/bunker/SettingsFragment.kt
+++ app/src/main/java/com/example/bunker/SettingsFragment.kt
@@ -1,11 +1,32 @@
 package com.example.bunker
 
 import android.os.Bundle
-import androidx.preference.PreferenceFragmentCompat
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.activityViewModels
+import com.example.bunker.databinding.FragmentMainMenuBinding
+import com.example.bunker.databinding.FragmentSettingsBinding
 
-class SettingsFragment : PreferenceFragmentCompat() {
+class SettingsFragment : Fragment() {
+    private val dataModel: DataModel by activityViewModels()
+    lateinit var binding: FragmentSettingsBinding
 
-    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val button = view.findViewById<Button>(R.id.back)
+        button.setOnClickListener{
+            dataModel.message.value = MainMenuFragment()
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSettingsBinding.inflate(inflater)
+        return binding.root
     }
 }
